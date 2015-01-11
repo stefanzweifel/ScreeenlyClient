565f527aca8dea1fddef5f8c9ceb031536c3175a
Stefan Zweifel <hello@stefanzweifel.io>
2015-01-11 21:45:23 +0100

    Added Doc Blocks

--- src/Wnx/ScreeenlyClient/Screenshot.php
+++ src/Wnx/ScreeenlyClient/Screenshot.php
@@ -1,26 +1,56 @@
 <?php namespace Wnx\ScreeenlyClient;
 
+use \GuzzleHttp\Client as Client;
+
 class Screenshot {
 
+    /**
+     * Screeenly API Key
+     * @var string
+     */
     protected $key;
 
+    /**
+     * Response from Screeenly
+     * @var object
+     */
     protected $response;
 
+    /**
+     * Request URL for Screenshot
+     * @var string
+     */
     protected $url;
 
+    /**
+     * Height
+     * @var integer
+     */
     protected $height;
 
+    /**
+     * Width
+     * @var integer
+     */
     protected $width = 1024;
 
+    /**
+     * @param string $key Screeenly API Key
+     */
     public function __construct($key)
     {
         $this->key = $key;
 
     }
 
+    /**
+     * Do API Request and store response
+     * @param  string $url
+     * @return object
+     */
     public function capture($url)
     {
-        $client = new \GuzzleHttp\Client();
+        $client = new Client();
         $response = $client->post('http://screeenly.com/api/v1/fullsize', ['body' =>
             [
                 'key'    => $this->key,
@@ -35,27 +65,53 @@ class Screenshot {
         return $this->response;
     }
 
+    /**
+     * Set height
+     * @param integer $height
+     */
     public function setHeight($height)
     {
+        if (is_numeric($height) === false) {
+            throw new \Exception('Height must be numeric', 1);
+        }
         $this->height = $height;
     }
 
+    /**
+     * Set width
+     * @param integer $width
+     */
     public function setWidth($width)
     {
+        if (is_numeric($width) === false) {
+            throw new \Exception('Width must be numeric', 1);
+        }
         $this->width = $width;
     }
 
+    /**
+     * Get Path
+     * @return string
+     */
     public function getPath()
     {
         return $this->response->path;
     }
 
+    /**
+     * Get base64 String
+     * @return string
+     */
     public function getBase64()
     {
         return $this->response->base64;
     }
 
-    public function store()
+    /**
+     * Store Screenshot no disk
+     * @param  string $storagePath
+     * @return string              Filename
+     */
     public function store($storagePath = '')
     {
         $path = $this->getPath();
