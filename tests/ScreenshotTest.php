f346754d30c9c5d041b7a10cb845967bd95fedf9
Stefan Zweifel <hello@stefanzweifel.io>
2015-01-24 18:05:21 +0100

    Added PhpUnit

---
+++ tests/ScreenshotTest.php
@@ -0,0 +1,69 @@
+<?php namespace Wnx\ScreeenlyClient;
+
+use Wnx\ScreeenlyClient\Screenshot;
+
+class ScreenshotTest extends \PHPUnit_Framework_TestCase {
+
+    public function testIsInstanceOfScreenshot()
+    {
+        $screenshot = new Screenshot('random-key');
+        $this->assertInstanceOf('Wnx\ScreeenlyClient\Screenshot', $screenshot);
+    }
+
+    public function testHasKeyAsAttribute()
+    {
+        $key        = 'random-key';
+        $screenshot = new Screenshot($key);
+
+        $this->assertObjectHasAttribute('key', $screenshot);
+        $this->assertAttributeEquals($key, 'key', $screenshot);
+    }
+
+    public function testHeightIsInteger()
+    {
+        $screenshot = new Screenshot('key');
+        $height = $screenshot->setHeight(1);
+
+        $this->assertEquals(1, $height);
+
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testHeightNotInteger()
+    {
+        $screenshot = new Screenshot('key');
+        $height = $screenshot->setHeight('string');
+    }
+
+    public function testWidthIsInteger()
+    {
+        $screenshot = new Screenshot('key');
+        $height = $screenshot->setWidth(1);
+
+        $this->assertEquals(1, $height);
+
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testWidthNotInteger()
+    {
+        $screenshot = new Screenshot('key');
+        $height = $screenshot->setWidth('string');
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testCaptureFails()
+    {
+        $screenshot = new Screenshot('key');
+        $screenshot->capture('http://google.com');
+    }
+
+
+}
+
